7bace7b4e89984337811bb59e6c69d66bf787b46
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-10-10 13:29:18 +0530

    Update functions.py

--- RenameBot/functions.py
+++ RenameBot/functions.py
@@ -8,6 +8,6 @@ async def progress(current, total, message, process):
     new_total = humanize.naturalsize(total)
     percentage = round((current * 100) / total, 2)
     try:
-        await message.edit(f"**{process}** \n\n**Progress :** {new_current}/{new_total} | {percentage}℅")
+        await message.edit(f"**{process}** \n\n**Progress :** {◽}{◾} | {percentage}℅")
     except FloodWait as e:
         time.sleep(e.x)
