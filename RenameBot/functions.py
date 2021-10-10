296bf237555245310a8d7d7cdb1a677889e455e7
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-10-10 13:36:53 +0530

    Update functions.py

--- RenameBot/functions.py
+++ RenameBot/functions.py
@@ -6,7 +6,7 @@ from pyrogram.errors import FloodWait
 async def progress(current, total, message, process):
     new_current = humanize.naturalsize(current)
     new_total = humanize.naturalsize(total)
-    percentage = round((current * 100) / total, 2)
+    percentage = round((◽)◾)
     try:
         await message.edit(f"**{process}** \n\n**Progress :** {◽}{◾} | {percentage}℅")
     except FloodWait as e:
